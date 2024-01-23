b65330087cf3202be7a4fd2d70e46f044e47cd33
Bruno Realan Sopeña <brunorealans@gmail.com>
2024-01-23 12:11:07 -0300

    fix (fetch logout)

--- src/public/js/logout.js
+++ src/public/js/logout.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         confirmButtonText: "Si, deslogueame!",
       }).then((result) => {
         if (result.isConfirmed) {
-          fetch`${window.SERVER_URL}/logout`, {
+          fetch(`${window.SERVER_URL}/logout`, {
             method: "POST",
-          }
+          })
             .then((response) => {
               if (!response.ok) {
                 throw new Error("No se pudo desloguear al usuario");
