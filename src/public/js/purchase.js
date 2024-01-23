c31105710868b4d1380dbed621320ccbf07afbc2
Bruno Realan Sopeña <brunorealans@gmail.com>
2024-01-23 20:21:10 -0300

    fix (double fetch on purchase.js)

--- src/public/js/purchase.js
+++ src/public/js/purchase.js
@@ -35,27 +35,6 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
       });
-      fetch(`${window.SERVER_URL}/api/carts/${cartId}/purchase`, {
-        method: "POST",
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("No se pudo crear el ticket de compra");
-          }
-          Swal.fire({
-            title: "Compra realizada correctamente",
-            text: "El ticket de compra ha sido creado, te contactaremos para el envio del producto.",
-            icon: "success",
-            showConfirmButton: false,
-            timer: 3000,
-          });
-          setTimeout(() => {
-            window.location.href = "/products";
-          }, 3000);
-        })
-        .catch((error) => {
-          console.error("Error al crear ticket de compra:", error);
-        });
     });
   }
 });
